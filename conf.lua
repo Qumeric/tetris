9872306e3b2f3aad1ca807af68e477fb4455b68c
qumeric <qumidium@gmail.com>
2014-08-04 22:50:33 +0400

    Almost playable. But with bugs

--- conf.lua
+++ conf.lua
@@ -1,7 +1,8 @@
 function love.conf(t)
+    t.author = 'Qumeric'   
     t.window.title = "Tetris"
     t.window.width = 360
-    t.window.height = 660
+    t.window.height = 720
 
     t.modules.joystick = false
     t.modules.physics = false
--- main.lua
+++ main.lua
@@ -1,37 +1,54 @@
+require 'SICK'
 Gamestate = require 'gamestate'
 
-SPEED = 3
+SPEED = 1
 
 -- Size of a block
 SIZE = 30
 
-HEIGHT = 20
+HEIGHT = 21
 WIDTH  = 10
 
 local menu = {}
 local game = {}
 
 -- Blocks are squares
-blocks = {{{1, 1, 1, 0}, {1, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}},
-          {{1, 1, 1, 1}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}},
-          {{1, 1, 1, 0}, {0, 1, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}},
-          {{0, 0, 1, 1}, {0, 1, 1, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}},
-          {{1, 1, 0, 0}, {0, 1, 1, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}},
+blocks = {{{0, 0, 0, 0}, {1, 1, 1, 0}, {1, 0, 0, 0}, {0, 0, 0, 0}},
+          {{0, 0, 0, 0}, {1, 1, 1, 1}, {0, 0, 0, 0}, {0, 0, 0, 0}},
+          {{0, 0, 0, 0}, {1, 1, 1, 0}, {0, 1, 0, 0}, {0, 0, 0, 0}},
+          {{0, 0, 0, 0}, {0, 0, 1, 1}, {0, 1, 1, 0}, {0, 0, 0, 0}},
+          {{0, 0, 0, 0}, {1, 1, 0, 0}, {0, 1, 1, 0}, {0, 0, 0, 0}},
           {{0, 0, 0, 0}, {0, 1, 1, 0}, {0, 1, 1, 0}, {0, 0, 0, 0}},
-          {{0, 1, 1, 1}, {0, 0, 0, 1}, {0, 0, 0, 0}, {0, 0, 0, 0}}}
+          {{0, 0, 0, 0}, {0, 1, 1, 1}, {0, 0, 0, 1}, {0, 0, 0, 0}}}
 
+
+function love.load()
+    highscore.set('highscores', 10, 'bot', 100)
+    Gamestate.registerEvents()
+    Gamestate.switch(menu)
+end
 
 function menu:draw()
+    love.graphics.setColor(255, 255, 255)
     love.graphics.print("Press Enter to continue", 10, 10)
+
+    for i, score, name in highscore() do
+        print(name)
+        love.graphics.print(name, 50, 50 + i * 30)
+        love.graphics.print(score, 200, 50 + i * 30)
+    end
 end
 
 function menu:keyreleased(key, code)
     if key == 'return' then
         Gamestate.switch(game)
+    elseif key == 'escape' then
+		love.event.quit()
     end
 end
 
-function game:enter()
+function game:enter()   
+    score = 0
     field = {}
 
     for i=1, HEIGHT do
@@ -76,6 +93,28 @@ function game:update(dt)
 
     if love.keyboard.isDown('down') then
         dtotal = dtotal + 1/SPEED
+    end
+
+    -- Check for full rows
+    for y=HEIGHT, 1, -1 do
+        for x=1, WIDTH do
+            if field[y][x] == 0 then break end
+            if x == WIDTH then game:destroy(y) end
+        end
+    end
+end
+
+function game:destroy(y)
+    for x=1, WIDTH do
+        field[y][x] = 0
+    end
+
+    score = score + 1
+
+    for y=y, 2, -1 do
+        for x=1, WIDTH do
+            field[y][x] = field[y-1][x]
+        end
     end
 end
 
@@ -128,23 +167,39 @@ function game:keypressed(key, code)
             end 
         end
 
-        -- DEBUG
-        for i=1, n do
-            print()
-            for j=1, n do
-                io.write(temp[i][j])
-            end
-        end
-        print()
-
+        local xshift = 0
 
         active_block = {}
         for i=1, n do
             for j=1, n do
+                local xpos = j+block_x
                 if block[i][j] == 1 then
-                    table.insert(active_block, {i+block_y, j+block_x})
+                    table.insert(active_block, {i+block_y, xpos})
+                    if xpos < 1 then
+                        xshift = math.max(-xpos+1, xshift)
+                    elseif xpos > WIDTH then
+                        xshift = math.min(WIDTH-xpos, xshift)
+                    end
                 end
             end
+        end
+
+        -- DEBUG
+        for i=1, n do
+            print()
+            for j=1, n do
+                io.write(block[i][j])
+            end
+        end
+        print()
+        
+
+        print('xshift= ' .. xshift)
+        if xshift ~= 0 then
+            for k, v in pairs(active_block) do
+                v[2] = v[2] + xshift  
+            end
+            block_x = block_x + xshift
         end
     end
 end
@@ -161,6 +216,8 @@ function game:spawn()
         for j=1, #block do
             if block[i][j] == 1 then
                 if field[i][j+3] == 1 then
+                    highscore.add('player', score)
+                    score = 0
                     Gamestate.switch(menu)
                 else
                     table.insert(active_block, {i, j+3})
@@ -187,14 +244,16 @@ function game:draw()
     love.graphics.setLineWidth(SIZE)
     love.graphics.rectangle('line', SIZE/2, SIZE/2, WIDTH*SIZE+SIZE, HEIGHT*SIZE+SIZE)
     
-    -- Draw active blockcd ..
+    -- Draw active block
     love.graphics.setColor(0, 127, 255)
     for _, i in pairs(active_block) do
         love.graphics.rectangle('fill', i[2]*SIZE, i[1]*SIZE, SIZE, SIZE)
     end
+
+    -- Draw score
+    love.graphics.print('Score: ' .. score, 10, 700)
 end
 
-function love.load()
-    Gamestate.registerEvents()
-    Gamestate.switch(menu)
+function love.quit()
+    highscore.save()
 end
